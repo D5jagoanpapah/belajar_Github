b0818430b9b2dfb03877fda2c82f8a68621aa244
D5jagoanpapah <145539263+D5jagoanpapah@users.noreply.github.com>
2023-09-20 15:07:05 +0700

    Create biodata.kt

---
+++ biodata.kt
@@ -0,0 +1,10 @@
+fun main ()
+{
+    var nama = "Hegar Isya"
+    var kelas = "XI RPL 2"
+    var hobi = "Menganalisis"
+
+  println("nama : $nama") 
+  println("kelas : $kelas") 
+  println("hobi : $hobi") 
+}
